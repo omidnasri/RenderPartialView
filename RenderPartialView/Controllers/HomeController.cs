4bd05352d56e26e2136e4b15507e1c6d5534c1b4
Omid Nasri <omid.nasri@outlook.com>
2015-12-31 20:17:46 +0330

    First Commit

---
+++ RenderPartialView/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RenderPartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult LoadData()
+        {
+            // your code.
+            return PartialView("_Page/_Alert");
+        }
+    }
+}
---
+++ RenderPartialView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RenderPartialView.MvcApplication" Language="C#" %>
